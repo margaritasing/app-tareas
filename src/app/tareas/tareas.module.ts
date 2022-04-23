8148939f9928a627128364a829e6d6d96ebffb07
margaritasing <libiamfreitesm@gmail.com>
2022-04-23 16:41:47 -0300

    agrego grid

--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubtitulosComponent } from './subtitulos/subtitulos.component';
 import { ListadoTareasComponent } from './listado-tareas/listado-tareas.component';
+import { GridComponent } from './grid/grid.component';
 
 
 
 @NgModule({
   declarations: [
     SubtitulosComponent,
-    ListadoTareasComponent
+    ListadoTareasComponent,
+    GridComponent
   ],
   imports: [
     CommonModule
